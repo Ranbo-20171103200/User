9a2961db706fd4c8b245373cf738c1b678a8342b
Ranbo-20171103200 <43673449+Ranbo-20171103200@users.noreply.github.com>
2019-06-29 23:14:37 +0800

    Add files via upload

---
+++ User.java
@@ -0,0 +1,23 @@
+package cn.edu.imnu.User;
+
+
+public class User {
+	private String Username;
+	private String password;
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
